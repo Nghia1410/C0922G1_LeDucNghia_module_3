e4f7d8903fef378a2bf7e91e90742e95498230e0
Ducnghia1410 <nghia14102003@gmail.com>
2022-12-19 17:09:32 +0700

    case study

--- src/ss2_thiet_ke_va_tao_co_so_du_lieu/exercise/selling_manager.sql
+++ src/ss2_thiet_ke_va_tao_co_so_du_lieu/exercise/selling_manager.sql
@@ -1,25 +1,34 @@
-create database quan_li_ban_hang;
+drop database if exists quan_li_ban_hang;
+create database if not exists quan_li_ban_hang;
 use quan_li_ban_hang;
-create table customer(
-customerid int primary key,
-customername varchar(45),
-customerage int
+
+CREATE TABLE customer (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(50),
+    customer_age INT
 );
-create table orders(
-orderid int primary key,
-customerid int,
-foreign key (customerid) references customer(customerid),
-orderdate date,
-ordertotalprice float
+
+CREATE TABLE `order` (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE,
+    order_total_price DOUBLE,
+    FOREIGN KEY (customer_id)
+        REFERENCES customer (customer_id)
 );
-create table product(
-productid int primary key,
-productname varchar(45),
-productprice float
+
+CREATE TABLE product (
+    product_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_name VARCHAR(50),
+    product_price DOUBLE
 );
-create table oderdetail(
-orderid int,
-productid int,
-foreign key (orderid)references orders(orderid),
-foreign key (productid)references product(productid)
+
+CREATE TABLE order_detail (
+    order_id INT,
+    product_id INT,
+    order_detail_qty INT,
+    FOREIGN KEY (order_id)
+        REFERENCES `order` (order_id),
+    FOREIGN KEY (product_id)
+        REFERENCES product (product_id)
 );
