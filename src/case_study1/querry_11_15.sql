64ba9bef0d193ebbe6b55b5e0b354c93ac4c3f74
Ducnghia1410 <nghia14102003@gmail.com>
2022-12-21 10:53:40 +0700

    case study

---
+++ src/case_study1/querry_11_15.sql
@@ -0,0 +1,37 @@
+use furama_resort;
+
+-- 11.	Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” 
+-- và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+select *from dich_vu_di_kem dvdk
+join hop_dong_chi_tiet hdct on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+join hop_dong hd on hd.ma_hop_dong = hdct.ma_hop_dong
+join khach_hang kh on kh.ma_khach_hang = hd.ma_khach_hang
+join loai_khach lk on lk.ma_loai_khach = kh.ma_loai_khach
+group by dvdk.ma_dich_vu_di_kem
+having ten_loai_khach like'%Diamond' and dia_chi like '%Vinh' or dia_chi like'%Quảng Ngãi'
+order by dvdk.ma_dich_vu_di_kem;
+
+-- 12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu,
+--  so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem),
+--  tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 
+-- nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+
+select hd.ma_hop_dong, nv.ho_ten, kh.ho_ten, kh.so_dien_thoai, dv.ten_dich_vu,  sum(ifnull(hdct.so_luong, 0)), hd.tien_dat_coc
+from hop_dong hd
+left join nhan_vien nv on hd.ma_nhan_vien = nv.ma_nhan_vien
+left join khach_hang kh on hd.ma_khach_hang = kh.ma_khach_hang 
+left join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
+left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+where 
+year(hd.ngay_lam_hop_dong)=2020
+and quarter(hd.ngay_lam_hop_dong)=4
+and hd.ngay_lam_hop_dong not in (
+year(hd.ngay_lam_hop_dong)=2021
+and quarter(hd.ngay_lam_hop_dong) in(1,2)
+)
+group by hd.ma_hop_dong;
+
+-- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng.
+--  (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+
