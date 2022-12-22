a902c0f4ec47c7d8bb521a6deb4d27d60d9cb0e1
Ducnghia1410 <nghia14102003@gmail.com>
2022-12-22 08:24:04 +0700

    case study

--- src/case_study1/querry_16_20.sql
+++ src/case_study1/querry_16_20.sql
@@ -11,3 +11,21 @@ where (year(hd.ngay_lam_hop_dong) in (2019,2020,2021))
 
 -- 17.	Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinum lên Diamond,
 --  chỉ cập nhật những khách hàng đã từng đặt phòng với Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ. 
+
+
+-- 18.	Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc giữa các bảng).
+SET FOREIGN_KEY_CHECKS=0;
+delete from khach_hang where khach_hang.ma_khach_hang
+in(
+select hop_dong.ma_khach_hang from hop_dong
+where year(hop_dong.ngay_lam_hop_dong)< 2021
+);
+
+-- 20.	Hiển thị thông tin của tất cả các nhân viên và khách hàng có trong hệ thống, thông tin hiển thị bao gồm
+--  id (ma_nhan_vien, ma_khach_hang), ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi.
+select ma_nhan_vien, ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi
+from nhan_vien
+union all
+select  ma_khach_hang, ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi
+from khach_hang
+group by khach_hang.ma_khach_hang
